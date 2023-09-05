a53051671a4ad075dc4f9b9f2d87b6fe128e8086
Kaosu-Niku <s9613010@gmail.com>
2023-09-05 16:12:55 +0800

    jQuery 效果

--- src/Pages/Jquery/JquerySyntax.js
+++ src/Pages/Jquery/JquerySyntax.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import $ from 'jquery';
 import './JquerySyntax.css';
 
 function JquerySyntax(props) {
+    const [effectOneTime, setEffectOneTime] = useState(0);
+
     return (
         <div className="flex main-content-div-iframe">
             <div className="flex content-div">
@@ -28,9 +31,9 @@ function JquerySyntax(props) {
                 <code>document.querySelector('#id').addEventListener('click',() ={'>'} {'{}'})</code>
             </div>
             <div className="flex content-div">
-                <h1>效果</h1>
+                <h1>動畫效果</h1>
                 <p>以下函式皆有以下參數可使用</p>
-                <p>參數1指定時間(單位為毫秒)，根據指定時間表現效果，參數2指定效果表現完後調用的callback函式</p>
+                <p>參數1指定時間(單位為毫秒)，根據指定時間表現過渡效果，參數2指定效果表現完後調用的callback函式</p>
                 <h3>show</h3>
                 <p>元素顯示</p>
                 <code>$('#id').show()</code>
@@ -39,16 +42,45 @@ function JquerySyntax(props) {
                 <code>$('#id').hide()</code>
                 <h3>toggle</h3>
                 <p>元素在顯示和隱藏狀態之間切換</p>
-                <code>$('#id').hide()</code>
+                <code>$('#id').toggle()</code>
+                <h3>fadeIn</h3>
+                <p>元素淡入顯示</p>
+                <code>$('#id').fadeIn()</code>
+                <h3>fadeOut</h3>
+                <p>元素淡出隱藏</p>
+                <code>$('#id').fadeOut()</code>
+                <h3>fadeToggle</h3>
+                <p>元素在淡入和淡出狀態之間切換</p>
+                <code>$('#id').fadeToggle()</code>
+                <h3>fadeTo</h3>
+                <p>元素過渡到一個特定的透明度</p>
+                <code>$('#id').fadeTo()</code>
+                <h3>slideDown</h3>
+                <p>元素向下展開</p>
+                <code>$('#id').slideDown()</code>
+                <h3>slideUp</h3>
+                <p>元素向上收縮</p>
+                <code>$('#id').slideUp()</code>
+                <h3>slideToggle</h3>
+                <p>元素在展開和收縮狀態之間切換</p>
+                <code>$('#id').slideToggle()</code>
                 <div className="function-div">
                     <h4>範例</h4>
-                    <button onClick={() => { $('.test-effect-one').show() }}>show</button>
+                    <p>效果:</p>
+                    <button className='effect-button' onClick={() => { $('.effect-result').show(effectOneTime) }}>show</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').hide(effectOneTime) }}>hide</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').toggle(effectOneTime) }}>toggle</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeIn(effectOneTime) }}>fadeIn</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeOut(effectOneTime) }}>fadeOut</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeToggle(effectOneTime) }}>fadeToggle</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideDown(effectOneTime) }}>slideDown</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideUp(effectOneTime) }}>slideUp</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideToggle(effectOneTime) }}>slideToggle</button>
                     <br></br>
-                    <button onClick={() => { $('.test-effect-one').hide() }}>hide</button>
+                    <p>動畫過渡時間:</p>
+                    <input type='number' value={effectOneTime} onChange={(event) => { setEffectOneTime(event.target.value) }}></input>
                     <br></br>
-                    <button onClick={() => { $('.test-effect-one').toggle() }}>toggle</button>
-                    <br></br>
-                    <div className='test-effect-one'></div>
+                    <div className='effect-result'></div>
                 </div>
             </div>
         </div>
