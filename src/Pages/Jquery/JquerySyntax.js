942bdefd450369d8a4ba79bb84900dc74d32c6a3
Kaosu-Niku <s9613010@gmail.com>
2023-09-08 16:00:35 +0800

    jQuery animation

--- src/Pages/Jquery/JquerySyntax.js
+++ src/Pages/Jquery/JquerySyntax.js
@@ -4,7 +4,8 @@ import $ from 'jquery';
 import './JquerySyntax.css';
 
 function JquerySyntax(props) {
-    const [effectOneTime, setEffectOneTime] = useState(0);
+    const [effectTime, setEffectTime] = useState(0);
+    const [customTime, setCustomTime] = useState(0);
 
     return (
         <div className="flex main-content-div-iframe">
@@ -32,8 +33,10 @@ function JquerySyntax(props) {
             </div>
             <div className="flex content-div">
                 <h1>動畫效果</h1>
-                <p>以下函式皆有以下參數可使用</p>
-                <p>參數1指定時間(單位為毫秒)，根據指定時間表現過渡效果，參數2指定效果表現完後調用的callback函式</p>
+                <p>以下是jQuery將常用的動畫效果包裝成的簡易函式，若是沒有想要的效果，則參考自定義動畫的部分</p>
+                <p>以下的函式皆有2個參數可使用</p>
+                <p>參數1指定時間(單位為毫秒)，根據指定時間表現動畫效果</p>
+                <p>參數2指定效果表現完後調用的callback函式</p>
                 <h3>show</h3>
                 <p>元素顯示</p>
                 <code>$('#id').show()</code>
@@ -66,21 +69,42 @@ function JquerySyntax(props) {
                 <code>$('#id').slideToggle()</code>
                 <div className="function-div">
                     <h4>範例</h4>
-                    <p>效果:</p>
-                    <button className='effect-button' onClick={() => { $('.effect-result').show(effectOneTime) }}>show</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').hide(effectOneTime) }}>hide</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').toggle(effectOneTime) }}>toggle</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').fadeIn(effectOneTime) }}>fadeIn</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').fadeOut(effectOneTime) }}>fadeOut</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').fadeToggle(effectOneTime) }}>fadeToggle</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').slideDown(effectOneTime) }}>slideDown</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').slideUp(effectOneTime) }}>slideUp</button>
-                    <button className='effect-button' onClick={() => { $('.effect-result').slideToggle(effectOneTime) }}>slideToggle</button>
+                    <p>動畫效果:</p>
+                    <button className='effect-button' onClick={() => { $('.effect-result').show(effectTime) }}>show</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').hide(effectTime) }}>hide</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').toggle(effectTime) }}>toggle</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeIn(effectTime) }}>fadeIn</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeOut(effectTime) }}>fadeOut</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').fadeToggle(effectTime) }}>fadeToggle</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideDown(effectTime) }}>slideDown</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideUp(effectTime) }}>slideUp</button>
+                    <button className='effect-button' onClick={() => { $('.effect-result').slideToggle(effectTime) }}>slideToggle</button>
                     <br></br>
                     <p>動畫過渡時間:</p>
-                    <input type='number' value={effectOneTime} onChange={(event) => { setEffectOneTime(event.target.value) }}></input>
+                    <input type='number' value={effectTime} onChange={(event) => { setEffectTime(event.target.value) }}></input>
                     <br></br>
                     <div className='effect-result'></div>
+                </div>
+            </div>
+            <div className="flex content-div">
+                <h1>自定義動畫效果</h1>
+                <p>該函式可以用CSS的屬性來自定義自己想要的動畫效果</p>
+                <code>$('#id').animate({'{'}params{'}'},speed,callback)</code>
+                <p>參數1指定要執行的動畫效果(CSS屬性) ex:{'{'}height: '250px'{'}'}</p>
+                <p>參數2指定時間(單位為毫秒)，根據指定時間表現動畫效果</p>
+                <p>參數3指定效果表現完後調用的callback函式</p>
+                <h3>show</h3>
+                <p>元素顯示</p>
+                <div className="function-div">
+                    <h4>範例</h4>
+                    <p>效果:</p>
+                    <button className='custom-button' onClick={() => { $('.custom-result').animate({width: '150px'},customTime) }}>width: '150px'</button>
+                    <button className='custom-button' onClick={() => { $('.custom-result').animate({width: '500px'},customTime) }}>width: '500px'</button>
+                    <br></br>
+                    <p>動畫過渡時間:</p>
+                    <input type='number' value={customTime} onChange={(event) => { setCustomTime(event.target.value) }}></input>
+                    <br></br>
+                    <div className='custom-result'></div>
                 </div>
             </div>
         </div>
